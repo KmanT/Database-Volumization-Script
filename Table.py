f07ed601389bca4d8e3a34bd159cd1d09c780532
KmanT <kturske@gmail.com>
2018-11-12 14:20:43 -0500

    Reads csv files & more volumetric stats
    Can now view initial volume and the program now reads a csv file

--- Table.py
+++ Table.py
@@ -4,12 +4,19 @@ class Table:
         self.avg_rec_size = avg_rec_size
         self.init_tab_size = init_tab_size
 
-    def calc_volumization(self, overhead, growth_rate, num_years):
+    def calc_initial_volume(self, overhead):
         answer = float(self.avg_rec_size) * float(self.init_tab_size)
         answer += answer * float(overhead)
 
+        return answer
+
+    def calc_growth_volume(self, overhead, growth_rate, num_years):
+        answer = self.calc_initial_volume(overhead, growth_rate, num_years)
+
         for i in range(1, num_years):
             answer += answer * float(growth_rate)
+
+        #answer += answer * (growth_rate * num_years)
 
         return answer
 
--- src.py
+++ src.py
@@ -4,41 +4,74 @@ overhead = 0.35
 growth_rate = 0.10
 num_years = 3
 
-more_tab = True
+
 
 tab_stack = []
 
 # Are there more tables?
-while more_tab:
+def console_input():
+    more_tab = True
+    while more_tab:
 
-    print("What is the name of your table?\n")
-    tab_name = input()
+        print("What is the name of your table?\n")
+        tab_name = input()
 
-    print("What is the average record length?\n")
-    avg_record_size = input()
+        print("What is the average record length?\n")
+        avg_record_size = input()
 
-    print("What is the table's initial size?\n")
-    init_tab_size = input()
+        print("What is the table's initial size?\n")
+        init_tab_size = input()
 
-    new_table = Table(tab_name, avg_record_size, init_tab_size)
-    tab_stack.append(new_table)
+        new_table = Table(tab_name, avg_record_size, init_tab_size)
+        tab_stack.append(new_table)
 
-    print("Are there more tables? (Y/N)")
-    answer = input()
+        print("Are there more tables? (Y/N)")
+        answer = input()
 
-    if answer == "N":
-        more_tab = False
+        if answer == "N":
+            more_tab = False
 
-
-total = 0
-print("\n\n\n TABLE VOLUMETRICS:")
-for table in tab_stack:
-    answer = table.calc_volumization(overhead, growth_rate, num_years)
-    print(table.tab_name + " : " + str(answer) + "\n")
-    total += answer
-
-
-print("--------------------------------------\n\n")
-print("Database Total: " + str(total))
+def read_input_csv():
+    input_file = open('input.csv', 'r')
+    for line in input_file:
+        tab_name = line.split(',')[0]
+        avg_record_size = line.split(',')[1]
+        init_tab_size = line.split(',')[2]
+        new_table = Table(tab_name, avg_record_size, init_tab_size)
+        tab_stack.append(new_table)
 
 
+def print_init_volumetrics():
+    total = 0
+    print("INITIAL TABLE VOLUMETRICS")
+    print("--------------------------------------\n")
+    for table in tab_stack:
+        answer = table.calc_initial_volume(overhead, growth_rate, num_years)
+        print(table.tab_name + " : " + str(answer) + "\n")
+        total += answer
+    print("--------------------------------------\n")
+    print("Database Total: " + str(total))
+    print("--------------------------------------\n")
+
+def print_growth_volumetrics():
+    total = 0
+    print("TABLE VOLUMETRICS AFTER " + str(num_years) + " YEARS")
+    print("--------------------------------------\n")
+    for table in tab_stack:
+        answer = table.calc_growth_volume(overhead, growth_rate, num_years)
+        print(table.tab_name + " : " + str(answer) + "\n")
+        total += answer
+    print("--------------------------------------\n")
+    print("Database Total: " + str(total))
+    print("--------------------------------------\n")
+
+def print_volumetrics():
+    print_init_volumetrics()
+    print_growth_volumetrics()
+
+#print("Are you going to input the table information yourself, or read input.csv?\n"
+#      + "'I' for input, 'R' for read.\n")
+
+
+read_input_csv()
+print_volumetrics()
